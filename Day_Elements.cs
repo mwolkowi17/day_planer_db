7bc1b9dc7f4db525a1bd763f48ef1104fe68e878
Marcin Wołkowicz <mwolkowi17@hotmail.com>
2019-10-14 21:07:44 +0200

    Second

---
+++ Day_Elements.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace day_planer_db
+{
+    class Day_Elements
+    {
+        public class DayContext : DbContext
+        {
+            public DbSet<Day> Days { get; set; }
+            
+
+            protected override void OnConfiguring(DbContextOptionsBuilder options)
+                => options.UseSqlite("Data Source=blogging.db");
+        }
+        public class Day
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace day_planer_db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new Day_Elements.DayContext())
+            {
+                Console.WriteLine("Wprowadź nowy tytuł wpisu:");
+                string a = Console.ReadLine();
+                Console.WriteLine("Wprowadź nowy wpis");
+                string b = Console.ReadLine();
+               
+                db.Days.Add(
+                    new Day_Elements.Day
+                    {
+                        Title = a,
+                        Content=b,
+                        
+                    }) ;
+                db.SaveChanges();
+            
+                
+            }
+        }
+    }
+}
