874150bafb35138274d72eb91d3260e8c41871dd
Marcin Wołkowicz <mwolkowi17@hotmail.com>
2019-10-19 22:00:04 +0200

    new menu

--- Program.cs
+++ Program.cs
@@ -9,9 +9,17 @@ namespace day_planer_db
         {
             using (var db = new Day_Elements.DayContext())
             {
+                
                 bool loop_main = true;
                 while (loop_main == true)
                 {
+                    Console.WriteLine("Jeśli chcesz wprowadzić nowy rekord wpisz: r");
+                    Console.WriteLine("Jeśli chcesz zobaczyć wpisy wprowadź: w");
+                    Console.WriteLine("Jeśli chcesz skasować jakiś element wprowadź: k");
+                    string decyzja_start = Console.ReadLine();
+                    //string decyzja_a = Console.ReadLine();
+                    //string decyzja_b = Console.ReadLine();
+                    if (decyzja_start == "r") { 
                     Console.WriteLine("Wprowadź nowy tytuł wpisu:");
                     string a = Console.ReadLine();
                     Console.WriteLine("Wprowadź nowy wpis");
@@ -25,11 +33,11 @@ namespace day_planer_db
 
                         });
                     db.SaveChanges();
-
-                    Console.WriteLine("Jeśli chcesz zobaczyć wpisy wprowadź: w");
-                    Console.WriteLine("Jeśli chcesz wprowadzić nowy wpis naciśni dowolny inny klawisz.");
-                    string decyzja_a = Console.ReadLine();
-                    if (decyzja_a == "w")
+                    }
+                   
+                    
+                    
+                    if (decyzja_start == "w")
                     {
                         var lista = db.Days.ToList();
                         foreach (var n in lista)
@@ -44,9 +52,9 @@ namespace day_planer_db
                         }
                     }
                     bool decyzja_C = false;
-                    Console.WriteLine("Jeśli chcesz skasować jakiś element wprowadź: k");
-                    string decyzja_b = Console.ReadLine();
-                    if (decyzja_b == "k")
+                    
+                    
+                    if (decyzja_start == "k")
                     {
                         decyzja_C = true;
                     }
@@ -59,6 +67,7 @@ namespace day_planer_db
                         var singlePost = db.Days
                                 .Single(b => b.Id == kas);
                         db.Days.Remove(singlePost);
+                        db.SaveChanges();
                         Console.WriteLine("chcesz skasować kolejny wpisz n:");
                         string decyzja_e = Console.ReadLine();
                         if (decyzja_e != "n")
